3f5206ae35bb59d5dec23d72c79397afe8d5607b
robert wilson <hotaru@thinkindifferent.net>
2010-12-28 07:08:23 -0500

    fix replacing words
    Change-Id: Icf1e3abe44e7b5ba4af51a5f21167858a9409b5d

--- java/src/net/thinkindifferent/inputmethod/latin/LatinIME.java
+++ java/src/net/thinkindifferent/inputmethod/latin/LatinIME.java
@@ -1995,6 +1995,9 @@ public class LatinIME extends InputMethodService
         InputConnection ic = getCurrentInputConnection();
         if (ic != null) {
             rememberReplacedWord(suggestion);
+            if (correcting) {
+                EditingUtil.deleteWordAtCursor(ic, mWordSeparators);
+            }
             ic.commitText(suggestion, 1);
         }
         saveWordInHistory(suggestion);
